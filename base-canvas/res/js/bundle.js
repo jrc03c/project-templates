17b0e5145375c6cfee2bf834ffd0d3ac8c234215
jrc03c <jrc03c@pm.me>
2023-12-25 20:25:41 -0600

    ...

--- base-canvas/res/js/bundle.js
+++ base-canvas/res/js/bundle.js
@@ -1,4 +1,12 @@
 (() => {
   // res/js/src/main.js
-  console.log("Hello, world!");
+  var width = 512;
+  var height = 512;
+  var canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  document.body.appendChild(canvas);
+  var context = canvas.getContext("2d");
+  context.fillStyle = "red";
+  context.fillRect(0, 0, width, height);
 })();
--- base-canvas/res/js/src/main.js
+++ base-canvas/res/js/src/main.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!")
+const width = 512
+const height = 512
+const canvas = document.createElement("canvas")
+canvas.width = width
+canvas.height = height
+document.body.appendChild(canvas)
+
+const context = canvas.getContext("2d")
+context.fillStyle = "red"
+context.fillRect(0, 0, width, height)
